4c4d8e22542a0ef03c5ab84ca295d27c58e561a9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-23 19:50:05 +0300

    Add 06DBAdvancedEFCoreAdvancedRelationsAndAggregation

---
+++ DBAdvancedEntityFrameworkCoreVI2018/06DBAdvancedEFCoreAdvancedRelationsAndAggregation/P01_BillsPaymentSystem.Data/EntityConfig/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_BillsPaymentSystem.Data.Models;
+
+namespace P01_BillsPaymentSystem.Data.EntityConfig
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.FirstName).IsUnicode();
+
+            builder.Property(u => u.LastName).IsUnicode();
+        }
+    }
+}
