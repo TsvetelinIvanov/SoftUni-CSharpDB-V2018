4948f6bb54fd41965eb176c89a883316f8c186ea
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-10 23:57:02 +0300

    Delete 09.sql

--- DBBasicsMSSQLV2018/ExamDB24VI2018/05_16Querying/09.sql
+++
@@ -1,6 +0,0 @@
-SELECT r.Id, r.Price, h.[Name] AS Hotel, c.[Name] AS City
-FROM Rooms AS r
-JOIN Hotels AS h ON h.Id = r.HotelId
-JOIN Cities AS c ON c.Id = h.CityId
-WHERE r.[Type] = 'First Class'
-ORDER BY r.Price DESC, r.Id
