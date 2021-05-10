6c32cebbc9ab04e4f0f6d37dbe6a17a41cf6572e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-10 23:56:26 +0300

    Delete 06.sql

--- DBBasicsMSSQLV2018/ExamDB24VI2018/05_16Querying/06.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-CASE WHEN MiddleName IS NOT NULL THEN CONCAT(FirstName, ' ', MiddleName, ' ', LastName) 
-ELSE CONCAT(FirstName, ' ', LastName)
-END AS [Full Name],
-YEAR(BirthDate) AS BirthYear
-FROM Accounts
-WHERE YEAR(BirthDate) > '1991'
-ORDER BY BirthYear DESC, [Full Name]
