40fa5d8e809d73b81cc5395ee034c5d61588ffe4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-08 23:23:24 +0300

    Update 07EmployeesWithProject.sql

--- DBBasicsMSSQLV2018/06SubqueriesAndJoins/01_11QueriesFromSoftUni/07EmployeesWithProject.sql
+++ DBBasicsMSSQLV2018/06SubqueriesAndJoins/01_11QueriesFromSoftUni/07EmployeesWithProject.sql
@@ -4,4 +4,3 @@ INNER JOIN EmployeesProjects AS e_p ON e_p.EmployeeID = e.EmployeeID
 INNER JOIN Projects AS p ON p.ProjectID = e_p.ProjectID
 WHERE p.StartDate > '08/13/2002' AND p.EndDate IS NULL
 ORDER BY e.EmployeeID
-
