9d657fb2db81107d70b89ff2f22cf945341bac6a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-09 00:09:31 +0300

    Update 13CountMountainRanges.sql

--- DBBasicsMSSQLV2018/06SubqueriesAndJoins/12_18QueriesFromGeography/13CountMountainRanges.sql
+++ DBBasicsMSSQLV2018/06SubqueriesAndJoins/12_18QueriesFromGeography/13CountMountainRanges.sql
@@ -4,14 +4,14 @@ JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
 WHERE c.CountryCode IN('BG', 'RU', 'US')
 GROUP BY c.CountryCode
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT mc.CountryCode, COUNT(m.MountainRange) AS MountainRanges
 FROM Mountains AS m
 JOIN MountainsCountries AS mc ON(mc.MountainId = m.Id AND mc.CountryCode IN('BG', 'RU', 'US'))
 GROUP BY mc.CountryCode
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT CountryCode, COUNT(MountainId) AS MountainRanges
 FROM MountainsCountries
