c3e7018a5d74e5f44b681394f28c8d04a494d131
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 23:18:17 +0300

    Update 17EmployeeSLoad_Programmability.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/17EmployeeSLoad_Programmability.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/17EmployeeSLoad_Programmability.sql
@@ -1,34 +1,32 @@
---The query below causes a compile time error in Jugge, but but in database work correctly!!!
+--The query below causes a compile time error in Jugge, but it works correctly in the database!!!
 CREATE FUNCTION udf_GetReportsCount(@employeeId INT, @statusId INT) 
 RETURNS INT AS
 BEGIN
  DECLARE @reportsCount INT
 
- SET @reportsCount = (SELECT COUNT(r.StatusId)
-                      FROM Employees AS e
+ SET @reportsCount = (SELECT COUNT(r.StatusId) FROM Employees AS e
                       JOIN Reports AS r ON r.EmployeeId = e.Id
-					  WHERE e.Id = @employeeId AND r.StatusId = @statusId
-					  GROUP BY r.EmployeeId)
+		      WHERE e.Id = @employeeId AND r.StatusId = @statusId
+		      GROUP BY r.EmployeeId)
 
  RETURN ISNULL(@reportsCount, 0)
 END
---The query above causes a compile time error in Jugge, but but in database work correctly!!!
+--The query above causes a compile time error in Jugge, but it works correctly in the database!!!
 
---In Judge must be paste only one query
+--In Judge must be pasted only one query
 
 CREATE FUNCTION udf_GetReportsCount(@employeeId INT, @statusId INT) 
 RETURNS INT AS
 BEGIN
  DECLARE @reportsCount INT
 
- SET @reportsCount = (SELECT COUNT(*) 
-                      FROM Reports 
-					  WHERE EmployeeId = @employeeId AND StatusId = @statusId)					  
+ SET @reportsCount = (SELECT COUNT(*) FROM Reports 
+		      WHERE EmployeeId = @employeeId AND StatusId = @statusId)					  
 
  RETURN @reportsCount
 END
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 SELECT Id, FirstName, Lastname, dbo.udf_GetReportsCount(Id, 2) AS ReportsCount
