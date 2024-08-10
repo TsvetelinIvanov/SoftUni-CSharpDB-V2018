6e2aea5f588d99e202e2707e067a784ad97b9dab
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 21:55:05 +0300

    Update 11EmergencyPatrol_Querying.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/11EmergencyPatrol_Querying.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/11EmergencyPatrol_Querying.sql
@@ -3,21 +3,20 @@ FROM Reports AS r
 JOIN Users AS u ON u.Id = r.UserId
 JOIN Categories AS c ON c.Id = r.CategoryId
 WHERE r.CloseDate IS NULL AND LEN(r.[Description]) > 20 AND r.[Description] LIKE '%str%' AND 
-      c.DepartmentId IN(1, 4, 5)     
+      c.DepartmentId IN (1, 4, 5)     
 ORDER BY r.OpenDate, [Reporter Email], r.Id
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT r.OpenDate, r.[Description], u.Email AS [Reporter Email]
 FROM Reports AS r
 JOIN Users AS u ON u.Id = r.UserId
 JOIN Categories AS c ON c.Id = r.CategoryId
 WHERE r.CloseDate IS NULL AND LEN(r.[Description]) > 20 AND r.[Description] LIKE '%str%' AND 
-      c.DepartmentId IN(SELECT Id FROM Departments 
-	  WHERE [Name] IN('Infrastructure', 'Emergency', 'Roads Maintenance'))     
+      c.DepartmentId IN (SELECT Id FROM Departments WHERE [Name] IN('Infrastructure', 'Emergency', 'Roads Maintenance'))     
 ORDER BY r.OpenDate, [Reporter Email], r.Id
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT r.OpenDate, r.[Description], u.Email AS [Reporter Email]
 FROM Reports AS r
@@ -25,16 +24,15 @@ JOIN Users AS u ON u.Id = r.UserId
 JOIN Categories AS c ON c.Id = r.CategoryId
 JOIN Departments AS d ON d.Id = c.DepartmentId
 WHERE r.CloseDate IS NULL AND LEN(r.[Description]) > 20 AND r.[Description] LIKE '%str%' AND       
-     d.[Name] IN('Infrastructure', 'Emergency', 'Roads Maintenance')
+     d.[Name] IN ('Infrastructure', 'Emergency', 'Roads Maintenance')
 ORDER BY r.OpenDate, [Reporter Email], r.Id
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT r.OpenDate, r.[Description], u.Email AS [Reporter Email]
 FROM Reports AS r
 JOIN Users AS u ON u.Id = r.UserId
-JOIN Categories AS c ON c.Id = r.CategoryId
-JOIN Departments AS d ON (d.Id = c.DepartmentId AND 
-d.[Name] IN('Infrastructure', 'Emergency', 'Roads Maintenance'))
+JOIN Categories AS c ON c.Id = r.CategoryId 
+JOIN Departments AS d ON (d.Id = c.DepartmentId AND d.[Name] IN ('Infrastructure', 'Emergency', 'Roads Maintenance'))
 WHERE r.CloseDate IS NULL AND LEN(r.[Description]) > 20 AND r.[Description] LIKE '%str%' 
 ORDER BY r.OpenDate, [Reporter Email], r.Id
