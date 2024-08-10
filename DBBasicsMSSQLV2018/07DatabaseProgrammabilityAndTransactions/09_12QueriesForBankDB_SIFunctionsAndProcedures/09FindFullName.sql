ab61602aefc7982f3c8d400f467295812eea6268
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 15:22:27 +0300

    Update 09FindFullName.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/09FindFullName.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/09FindFullName.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE usp_GetHoldersFullName AS
 SELECT FirstName + ' ' + LastName AS [Full Name]
 FROM AccountHolders
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXECUTE usp_GetHoldersFullName
