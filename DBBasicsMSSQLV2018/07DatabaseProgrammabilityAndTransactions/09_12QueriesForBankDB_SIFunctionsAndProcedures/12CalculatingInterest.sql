cd430cc8ce719dadeca9bfb71bb932a9cac88bed
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 15:34:44 +0300

    Update 12CalculatingInterest.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/12CalculatingInterest.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/12CalculatingInterest.sql
@@ -1,14 +1,14 @@
 CREATE PROCEDURE usp_CalculateFutureValueForAccount(@AccountId INT, @YearlyInterestRate FLOAT) AS
 BEGIN
-SELECT a.Id AS [Account Id], ah.FirstName AS [First Name], ah.LastName AS [Last Name], 
-a.Balance AS [Current balance], 
-dbo.ufn_CalculateFutureValue(a.Balance, @YearlyInterestRate, 5) AS [Balance in 5 years] 
-FROM Accounts AS a
-JOIN AccountHolders AS ah ON ah.Id = a.AccountHolderId
-WHERE a.Id = @AccountId
+  SELECT a.Id AS [Account Id], ah.FirstName AS [First Name], ah.LastName AS [Last Name], 
+  a.Balance AS [Current balance], 
+  dbo.ufn_CalculateFutureValue(a.Balance, @YearlyInterestRate, 5) AS [Balance in 5 years] 
+  FROM Accounts AS a
+  JOIN AccountHolders AS ah ON ah.Id = a.AccountHolderId
+  WHERE a.Id = @AccountId
 END
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXECUTE dbo.usp_CalculateFutureValueForAccount 1, 0.1
