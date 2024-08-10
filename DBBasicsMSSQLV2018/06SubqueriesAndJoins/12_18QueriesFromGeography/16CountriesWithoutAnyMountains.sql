f7d9f51dccd829cc880766e432b1af13651e444a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 11:09:06 +0300

    Update 16CountriesWithoutAnyMountains.sql

--- DBBasicsMSSQLV2018/06SubqueriesAndJoins/12_18QueriesFromGeography/16CountriesWithoutAnyMountains.sql
+++ DBBasicsMSSQLV2018/06SubqueriesAndJoins/12_18QueriesFromGeography/16CountriesWithoutAnyMountains.sql
@@ -3,7 +3,7 @@ FROM Countries AS c
 LEFT JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
 WHERE mc.MountainId IS NULL
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT COUNT(*) 
 FROM Countries 
