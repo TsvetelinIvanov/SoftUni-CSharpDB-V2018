67bea7f78dddfaff332236a9f4f6eaf52b43ad70
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 13:31:25 +0300

    Update 02EmployeesWithSalaryAboveNumber.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/02EmployeesWithSalaryAboveNumber.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/02EmployeesWithSalaryAboveNumber.sql
@@ -4,7 +4,7 @@ SELECT FirstName AS [First Name], LastName AS [Last Name]
 FROM Employees
 WHERE Salary >= @MinSalary
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXEC usp_GetEmployeesSalaryAboveNumber 48100
