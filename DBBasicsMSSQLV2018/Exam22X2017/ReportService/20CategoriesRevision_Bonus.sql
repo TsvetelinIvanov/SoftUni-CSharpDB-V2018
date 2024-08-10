124c23b18af8797325fe7d85371e7163ddd38886
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-11 00:27:27 +0300

    Update 20CategoriesRevision_Bonus.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/20CategoriesRevision_Bonus.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/20CategoriesRevision_Bonus.sql
@@ -1,23 +1,23 @@
 SELECT c.[Name] AS [Category Name], COUNT(r.Id) AS [Reports Number],
        CASE
-	    WHEN InProgressCount > WaitingCount THEN 'in progress'
-	    WHEN InProgressCount < WaitingCount THEN 'waiting'
-	    ELSE 'equal'
-	   END AS [Main Status]
+	WHEN InProgressCount > WaitingCount THEN 'in progress'
+	WHEN InProgressCount < WaitingCount THEN 'waiting'
+	ELSE 'equal'
+       END AS [Main Status]
 FROM Reports AS r
 JOIN Categories AS c ON c.Id = r.CategoryId
 JOIN [Status] AS s ON s.Id = r.StatusId
 JOIN (SELECT r.CategoryId, SUM(CASE WHEN s.Label = 'in progress' THEN 1 ELSE 0 END) AS InProgressCount,
-	         SUM(CASE WHEN s.Label = 'waiting' THEN 1 ELSE 0 END) AS WaitingCount
-	 FROM Reports AS r
-	 JOIN [Status] AS s ON s.Id = r.StatusId
-	 WHERE s.Label IN('waiting', 'in progress')
-	 GROUP BY r.CategoryId) AS sc ON sc.CategoryId = c.Id
-WHERE s.Label IN('waiting', 'in progress')
+      SUM(CASE WHEN s.Label = 'waiting' THEN 1 ELSE 0 END) AS WaitingCount
+      FROM Reports AS r
+      JOIN [Status] AS s ON s.Id = r.StatusId
+      WHERE s.Label IN ('waiting', 'in progress')
+      GROUP BY r.CategoryId) AS sc ON sc.CategoryId = c.Id
+WHERE s.Label IN ('waiting', 'in progress')
 GROUP BY c.[Name], 
-         CASE
-          WHEN InProgressCount > WaitingCount THEN 'in progress'
-          WHEN InProgressCount < WaitingCount THEN 'waiting'
-          ELSE 'equal'
-         END
+      CASE
+        WHEN InProgressCount > WaitingCount THEN 'in progress'
+        WHEN InProgressCount < WaitingCount THEN 'waiting'
+        ELSE 'equal'
+       END
 ORDER BY [Category Name], [Reports Number], [Main Status]
