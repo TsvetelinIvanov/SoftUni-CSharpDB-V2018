a49f9c0d85f608eb31c7158a2cec3764ac386117
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 18:29:24 +0300

    Update 04Delete_DML.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/04Delete_DML.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/04Delete_DML.sql
@@ -1,6 +1,6 @@
 DELETE FROM Reports WHERE StatusId = 4
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 DELETE FROM Reports 
 WHERE StatusId = (SELECT Id FROM [Status] WHERE Label = 'blocked')
