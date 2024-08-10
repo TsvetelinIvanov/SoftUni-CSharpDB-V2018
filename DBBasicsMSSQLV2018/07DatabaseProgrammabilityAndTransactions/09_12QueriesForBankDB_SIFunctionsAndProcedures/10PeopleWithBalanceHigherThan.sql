a564b7f7a42365ace3ecf00c62fd3fc7fab2be35
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 15:25:29 +0300

    Update 10PeopleWithBalanceHigherThan.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/10PeopleWithBalanceHigherThan.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/09_12QueriesForBankDB_SIFunctionsAndProcedures/10PeopleWithBalanceHigherThan.sql
@@ -5,7 +5,7 @@ JOIN Accounts AS a ON a.AccountHolderId = ah.Id
 GROUP BY ah.FirstName, ah.LastName
 HAVING SUM(a.Balance) > @balance
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXECUTE usp_GetHoldersWithBalanceHigherThan 1234.5667
