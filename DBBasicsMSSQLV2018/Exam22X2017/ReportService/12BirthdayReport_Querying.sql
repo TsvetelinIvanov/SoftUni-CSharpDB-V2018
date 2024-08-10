7b9a15f6f788f4aaf10d88ef4235da4eeadb72a1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 21:59:19 +0300

    Update 12BirthdayReport_Querying.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/12BirthdayReport_Querying.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/12BirthdayReport_Querying.sql
@@ -5,7 +5,7 @@ JOIN Users AS u ON (u.Id = r.UserId AND DAY(u.BirthDate) = DAY(r.OpenDate)
 AND MONTH(u.BirthDate) = MONTH(r.OpenDate))
 ORDER BY c.[Name]
 
---Only one query must be paste in Judge
+--Only one query must be pasted in Judge
 
 SELECT DISTINCT c.[Name] AS [Category Name]
 FROM Categories AS c
