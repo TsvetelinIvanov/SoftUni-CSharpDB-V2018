797863a283e9522d7d1e043b46db3a7c5bac2707
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 13:59:13 +0300

    Update 06EmployeesBySalaryLevel.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/06EmployeesBySalaryLevel.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/06EmployeesBySalaryLevel.sql
@@ -3,7 +3,7 @@ SELECT FirstName AS [First Name], LastName AS [Last Name]
 FROM Employees
 WHERE dbo.ufn_GetSalaryLevel(Salary) = @salaryLevel
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXEC usp_EmployeesBySalaryLevel  'High'
