8eac3489acb42276bc1eee7bbeb32029c0db388b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 13:25:47 +0300

    Update 01EmployeesWithSalaryAbove35000.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/01EmployeesWithSalaryAbove35000.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/01EmployeesWithSalaryAbove35000.sql
@@ -4,7 +4,7 @@ SELECT FirstName AS [First Name], LastName AS [Last Name]
 FROM Employees
 WHERE Salary > 35000
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 GO
 
 EXEC usp_GetEmployeesSalaryAbove35000 
