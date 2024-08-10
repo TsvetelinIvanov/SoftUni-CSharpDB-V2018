36a76555ea081fe60b9da105fb18c8bf92cc27a5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 15:36:42 +0300

    Update 05SalaryLevelFunction.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/05SalaryLevelFunction.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/05SalaryLevelFunction.sql
@@ -1,14 +1,15 @@
 CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18, 4))
 RETURNS CHAR(7) AS
 BEGIN
-DECLARE @salaryLevel CHAR(7)
-IF (@salary < 30000)
-SET @salaryLevel = 'Low'
-ELSE IF (@salary BETWEEN 30000 AND 50000)
-SET @salaryLevel = 'Average'
-ELSE
-SET @salaryLevel = 'High'
-RETURN @salaryLevel
+  DECLARE @salaryLevel CHAR(7)
+  IF (@salary < 30000)
+    SET @salaryLevel = 'Low'
+  ELSE IF (@salary BETWEEN 30000 AND 50000)
+    SET @salaryLevel = 'Average'
+  ELSE
+    SET @salaryLevel = 'High'
+  
+  RETURN @salaryLevel
 END
 
 --In Judge must be pasted without this below
