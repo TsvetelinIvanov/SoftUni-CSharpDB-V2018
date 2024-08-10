3e6914578a0ddc2c5e04124290f12c4527ce7be3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 22:22:39 +0300

    Update 15AverageClosingTime_Querying.sql

--- DBBasicsMSSQLV2018/Exam22X2017/ReportService/15AverageClosingTime_Querying.sql
+++ DBBasicsMSSQLV2018/Exam22X2017/ReportService/15AverageClosingTime_Querying.sql
@@ -1,5 +1,5 @@
 SELECT d.[Name] AS [Department Name],
-ISNULL(CONVERT(VARCHAR, AVG(DATEDIFF(DAY, r.OpenDate, r.CloseDate))), 'no info') AS [Aerage Duration]
+       ISNULL(CONVERT(VARCHAR, AVG(DATEDIFF(DAY, r.OpenDate, r.CloseDate))), 'no info') AS [Aerage Duration]
 FROM Departments AS d
 JOIN Categories AS c ON c.DepartmentId = d.Id
 LEFT JOIN Reports AS r ON r.CategoryId = c.Id
