2e7bbb649c5f9a8bb10bcdedaeb96570092aeef5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-10 15:04:41 +0300

    Update 08DeleteEmployeesAndDepartments.sql

--- DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/08DeleteEmployeesAndDepartments.sql
+++ DBBasicsMSSQLV2018/07DatabaseProgrammabilityAndTransactions/01_08QueriesForSoftUniDB_SIFunctionsProcedures/08DeleteEmployeesAndDepartments.sql
@@ -33,90 +33,82 @@ EXEC usp_DeleteEmployeesFromDepartment 3
 
 
 
-
 /* *****************************************************
      ***** NOT FOR JUDGE ***** NOT FOR JUDGE *****
 /* *****************************************************
-	Understood from the old exercise explanation
+	Understanding from the old exercise explanation
 /* *****************************************************
 	Problem 8.1. * Delete Employees and Departments
 ********************************************************/
 
-CREATE PROCEDURE usp_DeleteEmployeesFromDepartment
-(
-                 @departmentName NVARCHAR(50)
-)
-AS
-     BEGIN
-         ALTER TABLE Employees ALTER COLUMN ManagerID INT;
-         ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
-         UPDATE Employees
-           SET
-               DepartmentID = NULL
-         WHERE EmployeeID IN
-         (
-         (
-             SELECT e.EmployeeID
-             FROM Employees AS e
-                  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-             WHERE d.Name = @departmentName
-         )
-         );
-         UPDATE Employees
-           SET
-               ManagerID = NULL
-         WHERE ManagerID IN
-         (
-         (
-             SELECT e.EmployeeID
-             FROM Employees AS e
-                  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-             WHERE d.Name = @departmentName
-         )
-         );
-         ALTER TABLE Departments ALTER COLUMN ManagerID INT;
-         UPDATE Departments
-           SET
-               ManagerID = NULL
-         WHERE Name = @departmentName;
-         DELETE FROM Departments
-         WHERE Name = @departmentName;
-         DELETE FROM EmployeesProjects
-         WHERE EmployeeID IN
-         (
-         (
-             SELECT e.EmployeeID
-             FROM Employees AS e
-                  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-             WHERE d.Name = @departmentName
-         )
-         );
-         DELETE FROM Employees
-         WHERE EmployeeID IN
-         (
-         (
-             SELECT e.EmployeeID
-             FROM Employees AS e
-                  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-             WHERE d.Name = @departmentName
-         )
-         );
-     END;
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment(@departmentName NVARCHAR(50)) AS
+BEGIN
+  ALTER TABLE Employees ALTER COLUMN ManagerID INT;
+
+  ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
+
+  UPDATE Employees
+  SET DepartmentID = NULL WHERE EmployeeID IN 
+  (
+  (
+    SELECT e.EmployeeID FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+    WHERE d.Name = @departmentName
+  )
+  );
+
+  UPDATE Employees
+  SET ManagerID = NULL WHERE ManagerID IN
+  (
+  (
+    SELECT e.EmployeeID FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+    WHERE d.Name = @departmentName
+  )
+  );
+
+  ALTER TABLE Departments ALTER COLUMN ManagerID INT;
+  UPDATE Departments
+  SET ManagerID = NULL
+  WHERE Name = @departmentName;
+
+  DELETE FROM Departments
+  WHERE Name = @departmentName;
+
+  DELETE FROM EmployeesProjects
+  WHERE EmployeeID IN
+  (
+  (
+    SELECT e.EmployeeID FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+    WHERE d.Name = @departmentName
+  )
+  );
+
+  DELETE FROM Employees
+  WHERE EmployeeID IN
+  (
+  (
+    SELECT e.EmployeeID FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+    WHERE d.Name = @departmentName
+  )
+  );
+
+END;
          
 BEGIN TRANSACTION;
 
-EXECUTE usp_DeleteEmployeesFromDepartment
-        'Production';
+EXECUTE usp_DeleteEmployeesFromDepartment 'Production';
 
-EXECUTE usp_DeleteEmployeesFromDepartment
-        'Production Control';
+EXECUTE usp_DeleteEmployeesFromDepartment 'Production Control';
 
 ROLLBACK;
 
 /* *****************************************************
      ***** NOT FOR JUDGE ***** NOT FOR JUDGE *****
 /* *****************************************************
-	Understood from the old exercise explanation
+	Understanding from the old exercise explanation
 /* *****************************************************
 	Option without procedure 
 ********************************************************/
@@ -125,36 +117,31 @@ ALTER TABLE Employees ALTER COLUMN ManagerID INT;
 ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
 
 UPDATE Employees
-  SET
-      DepartmentID = NULL
+SET DepartmentID = NULL
 WHERE EmployeeID IN
 (
 (
-    SELECT e.EmployeeID
-    FROM Employees AS e
-         JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-    WHERE d.Name IN('Production', 'Production Control')
+  SELECT e.EmployeeID FROM Employees AS e
+  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+  WHERE d.Name IN('Production', 'Production Control')
 )
 );
 
 UPDATE Employees
-  SET
-      ManagerID = NULL
+SET  ManagerID = NULL
 WHERE ManagerID IN
 (
 (
-    SELECT e.EmployeeID
-    FROM Employees AS e
-         JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-    WHERE d.Name IN('Production', 'Production Control')
+  SELECT e.EmployeeID FROM Employees AS e
+  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+  WHERE d.Name IN('Production', 'Production Control')
 )
 );
 
 ALTER TABLE Departments ALTER COLUMN ManagerID INT;
 
 UPDATE Departments
-  SET
-      ManagerID = NULL
+SET ManagerID = NULL
 WHERE Name IN('Production', 'Production Control');
 
 DELETE FROM Departments
@@ -164,10 +151,10 @@ DELETE FROM EmployeesProjects
 WHERE EmployeeID IN
 (
 (
-    SELECT e.EmployeeID
-    FROM Employees AS e
-         JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-    WHERE d.Name IN('Production', 'Production Control')
+  SELECT e.EmployeeID
+  FROM Employees AS e
+  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+  WHERE d.Name IN('Production', 'Production Control')
 )
 );
 
@@ -175,10 +162,10 @@ DELETE FROM Employees
 WHERE EmployeeID IN
 (
 (
-    SELECT e.EmployeeID
-    FROM Employees AS e
-         JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-    WHERE d.Name IN('Production', 'Production Control')
+  SELECT e.EmployeeID
+  FROM Employees AS e
+  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+  WHERE d.Name IN('Production', 'Production Control')
 )
 );
 
@@ -190,65 +177,54 @@ WHERE EmployeeID IN
 	Problem 8.2. * Delete Employees and Departments
 ********************************************************/
 
-CREATE PROCEDURE usp_DeleteEmployeesFromDepartment
-(
-                 @departmentId INT
-)
-AS
-     BEGIN
-         ALTER TABLE Employees ALTER COLUMN ManagerID INT;
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment(@departmentId INT) AS
+BEGIN
+  ALTER TABLE Employees ALTER COLUMN ManagerID INT;
 
-         ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
+  ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
 
-         UPDATE Employees
-           SET
-               DepartmentID = NULL
-         WHERE EmployeeID IN
-         (
-         (
-             SELECT EmployeeID
-             FROM Employees
-             WHERE DepartmentID = @departmentId
-         )
-         );
+  UPDATE Employees
+  SET DepartmentID = NULL
+  WHERE EmployeeID IN
+  (
+  (
+    SELECT EmployeeID FROM Employees
+    WHERE DepartmentID = @departmentId
+  )
+  );
 
-         UPDATE Employees
-           SET
-               ManagerID = NULL
-         WHERE ManagerID IN
-         (
-         (
-             SELECT EmployeeID
-             FROM Employees
-             WHERE DepartmentID = @departmentId
-         )
-         );
+  UPDATE Employees
+  SET ManagerID = NULL
+  WHERE ManagerID IN
+  (
+  (
+    SELECT EmployeeID FROM Employees
+    WHERE DepartmentID = @departmentId
+  )
+  );
 
-         ALTER TABLE Departments ALTER COLUMN ManagerID INT;
+  ALTER TABLE Departments ALTER COLUMN ManagerID INT;
 
-         UPDATE Departments
-           SET
-               ManagerID = NULL
-         WHERE DepartmentID = @departmentId;
+  UPDATE Departments
+  SET ManagerID = NULL
+  WHERE DepartmentID = @departmentId;
 
-         DELETE FROM Departments
-         WHERE DepartmentID = @departmentId;
+  DELETE FROM Departments
+  WHERE DepartmentID = @departmentId;
 
-         DELETE FROM EmployeesProjects
-         WHERE EmployeeID IN
-         (
-         (
-             SELECT EmployeeID
-             FROM Employees
-             WHERE DepartmentID = @departmentId
-         )
-         );
+  DELETE FROM EmployeesProjects
+  WHERE EmployeeID IN
+  (
+  (
+    SELECT EmployeeID FROM Employees
+    WHERE DepartmentID = @departmentId
+  )
+  );
 
-         DELETE FROM Employees
-         WHERE DepartmentID = @departmentId;
+  DELETE FROM Employees
+  WHERE DepartmentID = @departmentId;
 
-         SELECT COUNT(*)
-         FROM Employees
-         WHERE DepartmentID = @departmentId;
-     END;
-	 
+  SELECT COUNT(*)
+  FROM Employees
+  WHERE DepartmentID = @departmentId;
+END;	 
