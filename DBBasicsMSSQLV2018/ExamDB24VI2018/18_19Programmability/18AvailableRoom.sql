7ded3a850bb8d7da7771bba6db2db1c123edb8b8
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-12 11:46:03 +0300

    Update 18AvailableRoom.sql

--- DBBasicsMSSQLV2018/ExamDB24VI2018/18_19Programmability/18AvailableRoom.sql
+++ DBBasicsMSSQLV2018/ExamDB24VI2018/18_19Programmability/18AvailableRoom.sql
@@ -3,21 +3,20 @@ RETURNS NVARCHAR(MAX) AS
 BEGIN
  DECLARE @BookedRooms TABLE(Id INT)
  INSERT INTO @BookedRooms
-  SELECT DISTINCT r.Id
-  FROM Rooms AS r
-  LEFT JOIN Trips AS t ON t.RoomId = r.Id
-  WHERE r.HotelId = @HotelId AND (@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND t.CancelDate IS NULL
+ SELECT DISTINCT r.Id
+ FROM Rooms AS r
+ LEFT JOIN Trips AS t ON t.RoomId = r.Id
+ WHERE r.HotelId = @HotelId AND (@Date BETWEEN t.ArrivalDate AND t.ReturnDate) AND t.CancelDate IS NULL
 
-DECLARE @Rooms TABLE(Id INT, Price DECIMAL(15, 2), Type VARCHAR(20), Beds INT, TotalPrice DECIMAL(15, 2))
-    INSERT INTO @Rooms 
-	SELECT TOP(1) r.Id, r.Price, r.[Type], r.Beds, @People * (h.BaseRate + r.Price) AS TotalPrice
-      FROM Rooms  AS r
-        LEFT JOIN Hotels AS h on h.Id = r.HotelId
-      WHERE r.HotelId = @HotelId AND r.Beds >= @People AND r.Id NOT IN (SELECT Id FROM @BookedRooms)
-      ORDER BY TotalPrice DESC
+ DECLARE @Rooms TABLE(Id INT, Price DECIMAL(15, 2), Type VARCHAR(20), Beds INT, TotalPrice DECIMAL(15, 2))
+ INSERT INTO @Rooms 
+ SELECT TOP(1) r.Id, r.Price, r.[Type], r.Beds, @People * (h.BaseRate + r.Price) AS TotalPrice
+ FROM Rooms  AS r
+ LEFT JOIN Hotels AS h on h.Id = r.HotelId
+ WHERE r.HotelId = @HotelId AND r.Beds >= @People AND r.Id NOT IN (SELECT Id FROM @BookedRooms)
+ ORDER BY TotalPrice DESC
 
  DECLARE @RoomCount INT = (SELECT COUNT(*) FROM @Rooms)
-
  IF (@RoomCount < 1)
  BEGIN
   RETURN 'No rooms available'
@@ -29,7 +28,7 @@ DECLARE @Rooms TABLE(Id INT, Price DECIMAL(15, 2), Type VARCHAR(20), Beds INT, T
  RETURN @Result
 END
 
---In Judge must be paste without this below
+--In Judge must be pasted without this below
 
 SELECT dbo.udf_GetAvailableRoom(112, '2011-12-17', 2)
 
