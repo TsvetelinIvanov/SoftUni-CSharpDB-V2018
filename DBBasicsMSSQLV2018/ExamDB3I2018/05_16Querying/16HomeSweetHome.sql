1c7bf3c904b100f9c2eb5d552a91787d4dd20504
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-12 17:37:34 +0300

    Update 16HomeSweetHome.sql

--- DBBasicsMSSQLV2018/ExamDB3I2018/05_16Querying/16HomeSweetHome.sql
+++ DBBasicsMSSQLV2018/ExamDB3I2018/05_16Querying/16HomeSweetHome.sql
@@ -1,24 +1,22 @@
 WITH cte_Rancs(ReturnOfficeId, OfficeId, Id, Manufacturer, Model)
-AS
-(SELECT ovm.ReturnOfficeId, ovm.OfficeId, ovm.Id, ovm.Manufacturer, ovm.Model
-FROM 
-	(SELECT DENSE_RANK() OVER(PARTITION BY v.Id ORDER BY o.CollectionDate DESC) AS LatestRentCarsRank,
-		  o.ReturnOfficeId, v.OfficeId, v.Id, m.Manufacturer, m.Model
-	FROM Orders AS o
-	RIGHT JOIN Vehicles AS v ON v.Id = o.VehicleId
-	JOIN Models AS m ON m.Id = v.ModelId
+AS 
+  (SELECT ovm.ReturnOfficeId, ovm.OfficeId, ovm.Id, ovm.Manufacturer, ovm.Model
+   FROM	(SELECT DENSE_RANK() OVER(PARTITION BY v.Id ORDER BY o.CollectionDate DESC) AS LatestRentCarsRank,
+	        o.ReturnOfficeId, v.OfficeId, v.Id, m.Manufacturer, m.Model
+	 FROM Orders AS o
+	 RIGHT JOIN Vehicles AS v ON v.Id = o.VehicleId
+	 JOIN Models AS m ON m.Id = v.ModelId
 	) AS ovm
-WHERE LatestRentCarsRank = 1)
+   WHERE LatestRentCarsRank = 1)
 
-SELECT CONCAT(Manufacturer, ' - ', Model) AS Vehicle,
-      [Location] = 
-	              CASE
-				   WHEN(SELECT COUNT(*) FROM Orders AS o WHERE o.VehicleId = cte_Rancs.Id) = 0 THEN 'home'
-				   WHEN(cte_Rancs.ReturnOfficeId IS NULL) THEN 'on a rent'
-				   WHEN(cte_Rancs.OfficeId <> cte_Rancs.ReturnOfficeId) 
-				   THEN (SELECT CONCAT(t.[Name], ' - ', o.[Name]) 
-				   FROM Towns AS t JOIN Offices AS o ON o.TownId = t.Id
-				   WHERE o.Id = cte_Rancs.ReturnOfficeId)
-				  END
+SELECT CONCAT(Manufacturer, ' - ', Model) AS Vehicle, [Location] = 
+	      CASE
+		WHEN(SELECT COUNT(*) FROM Orders AS o WHERE o.VehicleId = cte_Rancs.Id) = 0 THEN 'home'
+		WHEN(cte_Rancs.ReturnOfficeId IS NULL) THEN 'on a rent'
+		WHEN(cte_Rancs.OfficeId <> cte_Rancs.ReturnOfficeId) THEN (SELECT CONCAT(t.[Name], ' - ', o.[Name])
+								     FROM Towns AS t
+								     JOIN Offices AS o ON o.TownId = t.Id
+				   				     WHERE o.Id = cte_Rancs.ReturnOfficeId)
+	      END
 FROM cte_Rancs
 ORDER BY Vehicle, cte_Rancs.Id
