b617f9880c9e6ebe04c8a738d6769d96fa5ab52d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-12 15:52:06 +0300

    Update 08AvailableVehicles.sql

--- DBBasicsMSSQLV2018/ExamDB3I2018/05_16Querying/08AvailableVehicles.sql
+++ DBBasicsMSSQLV2018/ExamDB3I2018/05_16Querying/08AvailableVehicles.sql
@@ -1,6 +1,5 @@
 SELECT m.Model, m.Seats, v.Mileage
 FROM Models AS m
 JOIN Vehicles AS v ON v.ModelId = m.Id
-WHERE v.Id NOT IN 
-(SELECT o.VehicleId FROM Orders AS o WHERE o.ReturnDate IS NULL)
+WHERE v.Id NOT IN (SELECT o.VehicleId FROM Orders AS o WHERE o.ReturnDate IS NULL)
 ORDER BY v.Mileage ASC, m.Seats DESC, m.Id ASC
