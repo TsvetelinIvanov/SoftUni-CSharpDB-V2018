3f10365a9442beb088efa8084251d585ee715368
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-13 11:57:52 +0300

    Update 06RemoveVillain.sql

--- DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/06RemoveVillain.sql
+++ DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/06RemoveVillain.sql
@@ -1,7 +1,11 @@
-SELECT Id FROM Villains WHERE Id = 1
+SELECT Id FROM Villains
+WHERE Id = 1
 
-DELETE FROM MinionsVillains WHERE VillainId = 1
+DELETE FROM MinionsVillains
+WHERE VillainId = 1
 
-SELECT [Name] FROM Villains WHERE Id = 1
+SELECT [Name] FROM Villains
+WHERE Id = 1
 
-DELETE FROM Villains WHERE Id = 1
+DELETE FROM Villains
+WHERE Id = 1
