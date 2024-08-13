0f7aa34c707d118bae696f4ff3a102fa06521be9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-13 12:00:40 +0300

    Update 08IncreaseMinionAge.sql

--- DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/08IncreaseMinionAge.sql
+++ DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/08IncreaseMinionAge.sql
@@ -5,12 +5,17 @@ SET [Name] = LOWER([Name])
 
 UPDATE Minions
 SET [Name] = 'bob kevin'
-WHERE Id IN(17, 13, 12)
+WHERE Id IN (17, 13, 12)
 
-UPDATE Minions SET Age += 1 WHERE Id = 1
+UPDATE Minions
+SET Age += 1
+WHERE Id = 1
 
-SELECT [Name] FROM Minions WHERE Id = 17
+SELECT [Name] FROM Minions
+WHERE Id = 17
 
-UPDATE Minions SET [Name] = 'Bob Kevin' WHERE Id = 13
+UPDATE Minions
+SET [Name] = 'Bob Kevin'
+WHERE Id = 13
 
 SELECT [Name], Age FROM Minions
