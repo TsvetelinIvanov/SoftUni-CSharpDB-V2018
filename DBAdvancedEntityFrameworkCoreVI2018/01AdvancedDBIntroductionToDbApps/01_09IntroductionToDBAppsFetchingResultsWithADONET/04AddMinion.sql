dfd387252549fb6a906365f4433d762329786768
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-13 11:55:07 +0300

    Update 04AddMinion.sql

--- DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/04AddMinion.sql
+++ DBAdvancedEntityFrameworkCoreVI2018/01AdvancedDBIntroductionToDbApps/01_09IntroductionToDBAppsFetchingResultsWithADONET/04AddMinion.sql
@@ -2,12 +2,20 @@ SELECT Id
 FROM Towns
 WHERE [Name] = 'Sofia'
 
-INSERT INTO Towns ([Name]) VALUES ('Montana')
+INSERT INTO Towns ([Name])
+VALUES
+('Montana')
 
-SELECT Id FROM Villains WHERE[Name] = 'Jilly'
+SELECT Id FROM Villains
+WHERE[Name] = 'Jilly'
 
-INSERT INTO Minions ([Name], Age, TownId) VALUES ('Torn', 47, 3)
+INSERT INTO Minions ([Name], Age, TownId)
+VALUES
+('Torn', 47, 3)
 
-SELECT Id FROM Minions WHERE [Name] = 'Torn'
+SELECT Id FROM Minions
+WHERE [Name] = 'Torn'
 
-INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (1, 6)
+INSERT INTO MinionsVillains (MinionId, VillainId)
+VALUES
+(1, 6)
