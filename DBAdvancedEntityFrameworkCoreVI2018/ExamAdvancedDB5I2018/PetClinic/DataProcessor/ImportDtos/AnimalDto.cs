e81d2606dd0574c5e1c17f7f1ad09cf7dd7a3297
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-13 09:50:15 +0300

    Restore AnimalDto.cs

---
+++ DBAdvancedEntityFrameworkCoreVI2018/ExamAdvancedDB5I2018/PetClinic/DataProcessor/ImportDtos/AnimalDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PetClinic.DataProcessor.ImportDtos
+{
+    public class AnimalDto
+    {        
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Type { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Age { get; set; }
+
+        [Required]
+        public PassportDto Passport { get; set; }
+    }
+}
