aab36ccfbc6c071394ffb1e476a2375c3e36f6f6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-14 12:00:39 +0300

    Update EmployeeProject.cs

--- DBAdvancedEntityFrameworkCoreVI2018/03DBAdvancedIntroductionToEntityFrameworkCore/P02_DatabaseFirst/Data/Models/EmployeeProject.cs
+++ DBAdvancedEntityFrameworkCoreVI2018/03DBAdvancedIntroductionToEntityFrameworkCore/P02_DatabaseFirst/Data/Models/EmployeeProject.cs
@@ -2,10 +2,10 @@
 {
     public class EmployeeProject
     {
-        public int EmployeeId { get; set; }
-        public int ProjectId { get; set; }
-
+        public int EmployeeId { get; set; }        
         public Employee Employee { get; set; }
+
+        public int ProjectId { get; set; }
         public Project Project { get; set; }
     }
 }
