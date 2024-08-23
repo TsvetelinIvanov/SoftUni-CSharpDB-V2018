bcb97f2f288720d4698e5df3d53c6182ec539c1f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-23 19:43:49 +0300

    Delete DBAdvancedEntityFrameworkCoreVI2018/ExamAdcanvedDB5I2018/PetClinic directory

--- DBAdvancedEntityFrameworkCoreVI2018/ExamAdcanvedDB5I2018/PetClinic/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PetClinic.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-UGR754R\SQLEXPRESS;Database=PetClinic;Trusted_Connection=True";
-    }
-}
