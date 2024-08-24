b171420f896cc23d713da239035631f266cc3144
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-24 05:52:23 +0300

    Update FastFoodDbContext.cs

--- DBAdvancedEntityFrameworkCoreVI2018/ExamAdvancedDB10XII2017/FastFood.Data/FastFoodDbContext.cs
+++ DBAdvancedEntityFrameworkCoreVI2018/ExamAdvancedDB10XII2017/FastFood.Data/FastFoodDbContext.cs
@@ -3,17 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FastFood.Data
 {
-	public class FastFoodDbContext : DbContext
+    public class FastFoodDbContext : DbContext
+    {
+	public FastFoodDbContext()
 	{
-		public FastFoodDbContext()
-		{
 
-		}
+	}
 
-		public FastFoodDbContext(DbContextOptions options) : base(options)
-		{
+	public FastFoodDbContext(DbContextOptions options) : base(options)
+	{
 
-		}
+	}
         
         public DbSet<Employee> Employees { get; set; }
 
@@ -27,31 +27,31 @@ namespace FastFood.Data
 
         public DbSet<Category> Categories { get; set; }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder builder)
-		{
-			if (!builder.IsConfigured)
-			{
-				builder.UseSqlServer(Configuration.ConnectionString);
-			}
-		}
+	protected override void OnConfiguring(DbContextOptionsBuilder builder)
+	{
+	    if (!builder.IsConfigured)
+	    {
+		builder.UseSqlServer(Configuration.ConnectionString);
+	    }
+	}
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
             builder.Entity<OrderItem>().HasKey(oi => new { oi.OrderId, oi.ItemId });
 
             builder.Entity<OrderItem>().HasOne(oi => oi.Order)
                 .WithMany(o => o.OrderItems)
                 .HasForeignKey(oi => oi.OrderId);
-            //.OnDelete(DeleteBehavior.Restrict);
+              //.OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<OrderItem>().HasOne(oi => oi.Item)
                 .WithMany(i => i.OrderItems)
                 .HasForeignKey(oi => oi.ItemId);
-                //.OnDelete(DeleteBehavior.Restrict);
+              //.OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<Item>().HasIndex(i => i.Name).IsUnique();
 
             builder.Entity<Position>().HasIndex(p => p.Name).IsUnique();
-		}
 	}
+    }
 }
